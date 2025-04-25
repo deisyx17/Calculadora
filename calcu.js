3171651037da81838edc8abcf34956bbf581ad77
deisyx17 <deisy.deisy1134@gmail.com>
2025-04-25 16:47:13 -0300

    Add files via upload

---
+++ calcu.js
@@ -0,0 +1,20 @@
+function selecionar(valor){
+    let selecionar = document.getElementById('i')
+    i.value += valor
+}
+
+
+    
+function igualar(){
+    document.getElementById('i').value = eval(Number)
+}
+
+function limpar(){
+    document.getElementById('i').value = '';
+  }
+
+
+
+
+
+
